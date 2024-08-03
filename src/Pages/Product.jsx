ffe62dc637dc29a68c537a0ddabb29613091fe6b
Sonu Lodhi <sonulodhis142@gmail.com>
2024-08-03 11:32:31 +0530

    all file

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import './CSS/Product.css'
+import {ShopContaxt} from '../Contaxt/ShopContaxt'
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/breadcrums/breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+
+const Product = () =>{
+    const {all_product} =useContext(ShopContaxt);
+    const {productId} = useParams();
+    const product = all_product.find((e)=>e.id===Number(productId))
+    return (
+        <div className="product">
+            <Breadcrum product={product}/>
+            <ProductDisplay product={product}/>
+        </div>
+    )
+}
+
+export default Product;

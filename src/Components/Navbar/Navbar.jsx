c07b0108e4da1e790cae8be2414c2fa9d2714438
Sonu Lodhi <sp95759201@gmail.com>
2025-10-13 13:29:20 +0530

    new text

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 import { Link } from "react-router-dom";
 
 const Navbar = ( ) => {
-
+            // new text to write here
     return(
         <div className="navbar">
             <div className="navbar-logo">
